111543d17a0558cf4b66c05d3094db02053396c2
Arman <arman@moi.sh>
2024-10-20 00:26:05 -0400

    Updated with latest files

---
+++ nixos/screen.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  services.geoclue2.appConfig = {
+      "gammastep" = {
+        isAllowed = true;
+        isSystem = false;
+        users = [ "1000" ]; 
+      };
+  };
+  
+  programs.light.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # wlsunset
+    gammastep
+    brightnessctl
+  ];
+}
---
+++ nixos/utils.nix
@@ -0,0 +1,23 @@
+{ pkgs, inputs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    mold
+    gcc13
+    jdk11
+    dioxus-cli
+    surrealdb
+    surrealdb-migrations
+    surrealist
+    trunk
+    alacritty
+    steam-run
+    fuse
+    inotify-tools
+    unzip
+    inputs.Neve.packages.${pkgs.system}.default
+    noto-fonts
+    dejavu_fonts
+    bun
+  ];
+}
